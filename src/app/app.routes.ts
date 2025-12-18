cf1cfcf81239f5e8308e1fec13a6329347791a3e
DarelleHapket <darellehapket@gmail.com>
2025-12-18 05:01:42 +0100

    Auth: login, localStorage, AuthGuard, routing admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//  ceci c'est la route par defaut si on a rien ca redirige vers login 
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+
+  
+
+  { path: 'login', component: LoginComponent },
+
+    // Groupe de routes ADMIN protégées
+  {
+    path: 'admin',
+
+    // canActivate : Angular appelle AuthGuard
+    // AVANT d’autoriser l’accès à /admin/*
+    canActivate: [AuthGuard],
+//les routes enfants de admin
+    children: [
+      { 
+        path: 'dashboard', component: DashboardComponent 
+      }
+    ]
+  },
+
+     // Route inconnue → retour au login
+  { 
+    path: '**', 
+    redirectTo: 'login' 
+  },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  // function login
+  login() {
+    console.log('Login cliqué', this.email, this.password);
+
+    if (!this.email || !this.password) {
+      alert('Veuillez remplir tous les champs');
+      return;
+    }
+
+    // Simulation de login
+    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem('userEmail', this.email);
+
+    console.log('LocalStorage OK');
+    this.router.navigate(['/admin/dashboard']);// redirection vers le dashboard admin
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
